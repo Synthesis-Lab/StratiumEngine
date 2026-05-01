d6233942d2a313f9806ef0420d76f6767830a8d6
Berkay74-67 <berkayyberka@gmail.com>
2026-05-01 21:22:04 +0300

    Proje dosyası ekle.

---
+++ StratiumEngine.Core/StratiumEngine.Core.cpp
@@ -0,0 +1,12 @@
+﻿// StratiumEngine.Core.cpp: Uygulamanın giriş noktasını tanımlar.
+//
+
+#include "StratiumEngine.Core.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
